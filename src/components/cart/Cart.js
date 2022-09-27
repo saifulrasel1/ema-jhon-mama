ac14f9564582a936852328b89a98c4af5e534a1d
saifulrasel <saifulrox11223344@gmail.com>
2022-09-27 20:59:20 +0600

    added

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+    const {cart} = props;
+    console.log(cart)
     return (
-        <div>
+        <div className='cart'>
            <h1>this is cart</h1>
                 <p>Selected-Item:{cart.length}</p> 
+                <p>Totall price:</p>
+                <p>Totall shipping:</p>
+                <p>Tax:</p>
+                <h4>Grand Totall:</h4>
         </div>
     );
 };
